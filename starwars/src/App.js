44ec4b68d0a42885164b4f43a9b5c8a0a600f018
Mica <48641666+micarsls@users.noreply.github.com>
2022-09-18 23:31:14 +0400

    useQuery Hook

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Planets from "./components/Planets";
 import People from "./components/People";
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
 
 function App() {
 
@@ -10,9 +12,11 @@ function App() {
     <div className="App">
       <h1>Star Wars Info</h1>
       <Navbar setPage={setPage}/>
-      <div className="content">
-        { page === 'planets' ? <Planets/> : <People/>}
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="content">
+          { page === 'planets' ? <Planets/> : <People/>}
+        </div>
+      </QueryClientProvider>
     </div>
   );
 }
--- starwars/src/components/Planets.js
+++ starwars/src/components/Planets.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Planet from "./Planet";
+
+const fetchPlanets = async () => {
+  const res = await fetch('https://swapi.dev/api/planets/');
+  return res.json();
+};
 
 const Planets = () => {
+    const { data, status } = useQuery('planets', fetchPlanets);
+    console.log(data);
+
     return (
         <div>
             <h2>Planets</h2>
+            {/* <p>{ status }</p> */}
+
+            {status === 'loading' && (
+                <div>Loading data...</div>
+            )}
+
+            {status === 'error' && (
+                <div>Error fetching data</div>
+            )}
+
+            {status === 'success' && (
+                <div>
+                    {data.results.map(planet => <Planet planet={planet} key={planet.name}/>)}
+                </div>
+            )}
         </div>
     );
 }
