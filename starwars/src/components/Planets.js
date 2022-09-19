dc9e83a48369a703c66b7992c41a706a45275b08
Mica <48641666+micarsls@users.noreply.github.com>
2022-09-19 16:02:43 +0400

    Query Variables

--- starwars/src/components/Planets.js
+++ starwars/src/components/Planets.js
@@ -1,24 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import Planet from "./Planet";
 
-const fetchPlanets = async () => {
-  const res = await fetch('https://swapi.dev/api/planets/');
+const fetchPlanets = async (queryKey, page) => {
+  const res = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 };
 
 const Planets = () => {
-    const { data, status } = useQuery(['planets'], fetchPlanets, {
-        staleTime: 0,
-        // cacheTime: 10
-        onSuccess: () => console.log('data fetched with no problemo')
-    });
-    console.log(data);
+    const [page, setPage] = useState(1);
+    const { data, status } = useQuery(["planets", page], ({ queryKey }) => fetchPlanets(queryKey, page));
 
     return (
         <div>
             <h2>Planets</h2>
-            {/* <p>{ status }</p> */}
+            
+            <button onClick={() => setPage(1)}>Page 1</button>
+            <button onClick={() => setPage(2)}>Page 2</button>
+            <button onClick={() => setPage(3)}>Page 3</button>
 
             {status === 'loading' && (
                 <div>Loading data...</div>
